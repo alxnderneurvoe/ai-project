af650c23cf65371a038183333c049e5e35236791
alxnderneurvoe <mhabibillah2303@gmail.com>
2026-07-18 22:23:45 +0700

    .

--- config/webhook.php
+++ config/webhook.php
@@ -1,16 +1,16 @@
 <?php
 
 // CRUD DEVICE 
-define('WEBHOOK_DEVICE', 'http://localhost:5678/webhook/get-device');
-define('WEBHOOK_ADD_DEVICE', 'http://localhost:5678/webhook/add-device');
-define('WEBHOOK_UPDATE_DEVICE', 'http://localhost:5678/webhook/update-device');
-define('WEBHOOK_DELETE_DEVICE', 'http://localhost:5678/webhook/delete-device');
+define('WEBHOOK_DEVICE', 'http://n8n.alxnderneurvoe.xyz/webhook/get-device');
+define('WEBHOOK_ADD_DEVICE', 'http://n8n.alxnderneurvoe.xyz/webhook/add-device');
+define('WEBHOOK_UPDATE_DEVICE', 'http://n8n.alxnderneurvoe.xyz/webhook/update-device');
+define('WEBHOOK_DELETE_DEVICE', 'http://n8n.alxnderneurvoe.xyz/webhook/delete-device');
 
 // FLOW HANDOVER
-define('WEBHOOK_UPDATE', 'http://localhost:5678/webhook/update-handover');
+define('WEBHOOK_UPDATE', 'http://n8n.alxnderneurvoe.xyz/webhook/update-handover');
 
 // HISTORY
-define('WEBHOOK_HISTORY', 'http://localhost:5678/webhook/get-history');
+define('WEBHOOK_HISTORY', 'http://n8n.alxnderneurvoe.xyz/webhook/get-history');
 
 //BELUM TERPAKAI
-define('WEBHOOK_ASSIGNED', 'http://localhost:5678/webhook/get-device-assigned');
+define('WEBHOOK_ASSIGNED', 'http://n8n.alxnderneurvoe.xyz/webhook/get-device-assigned');
